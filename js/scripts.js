fba5b0747a6f8af1efa1e3a9fd1cb0dc7008cb1a
Mark Helt and Riki Montgomery <mark@>
2017-08-10 08:56:38 -0700

    Add html, css, and js for a voting information website.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,18 @@
 $(document).ready(function() {
-//   var age = parseInt(prompt("How old are you?"));
+  var age = parseInt(prompt("How old are you?"));
+
+  if (age >= 18) {
+    $('#ofVotingAge').show();
+  }   else {
+    $('#notOfVotingAge').show();
+  }
+});
+
+// var over21 = confirm("Are you over 21? Click OK for yes or Cancel for no.");
 //
-//   if (age > 21) {
-//     $('#drinks').show();
-//   } else if (age === 21){
-//     alert("Now don't go crazy!");
+//   if (over21) {
 //     $('#drinks').show();
 //   } else {
 //     $('#under-21').show();
 //   }
 // });
-
-var over21 = confirm("Are you over 21? Click OK for yes or Cancel for no.");
-
-  if (over21) {
-    $('#drinks').show();
-  } else {
-    $('#under-21').show();
-  }
-});
